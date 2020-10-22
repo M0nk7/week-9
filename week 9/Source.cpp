5b5a8dd2f13983b3aa2e0884e4a425aab658e3ff
M0nk7 <63010332@kmitl.ac.th>
2020-10-22 21:38:58 +0700

    Add project files.

---
+++ week 9/Source.cpp
@@ -0,0 +1,18 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main()
+{
+    char arr[100000];
+    int i = 0;
+	int big = 0, small = 0;
+    scanf("%[^\n]",arr);
+	while(arr[i]!='\0')
+	{
+		if (arr[i] >= 'A' && arr[i] <= 'Z') big++;
+		else if (arr[i] >= 'a' && arr[i] <= 'z') small++;
+		i++;
+	}
+	printf("Capital letter = %d\nLower-case letter = %d",big,small);
+    return 0;
+}
